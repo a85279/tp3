ee49f80a8a4ee12494ebc0148c4ae4e0127fb8ab
Eduardo Alvito <126621486+AlvitoEduardo@users.noreply.github.com>
2023-12-21 23:06:04 +0000

    Ficheiro docusaurus.config.js alterado

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   projectName: 'tp3', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
